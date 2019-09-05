6f4e38c64f275e5751586ec16f8737af2b94fb2c
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-09-05 12:10:03 -0700

    logic works, need to set up components and add tests

--- src/App.js
+++ src/App.js
@@ -1,10 +1,56 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [ballCount, setBallCount] = useState(0)
+  const [strikeCount, setStrikeCount] = useState(0)
+
+  const handleBall = () => {
+    if (strikeCount === 2 || ballCount === 3) {
+      setBallCount(0)
+      setStrikeCount(0)
+    } else {
+      setBallCount(ballCount + 1)
+    }
+  }
+
+  const handleStrike = () => {
+    if (strikeCount === 2 || ballCount === 3) {
+      setBallCount(0)
+      setStrikeCount(0)
+    } else {
+      setStrikeCount(strikeCount + 1)
+    }
+  }
+
+  const handleFoul = () => {
+    if (strikeCount === 0) {
+      setStrikeCount(1)
+    } else if (strikeCount === 1) {
+      setStrikeCount(2)
+    } else if (strikeCount === 2) {
+      setStrikeCount(strikeCount)
+    }
+  }
+
+  const handleHit = () => {
+    setBallCount(0)
+    setStrikeCount(0)
+  }
+
+
   return (
-    <div className="App">
-      <p> hello world test</p>
+    <div className="container">
+      <section className="scoreboard">
+        <div className="ballCount">Ball: {ballCount}</div>
+        <div className="strikeCount">Strike: {strikeCount}</div>
+      </section>
+      <section className="buttons">
+        <button onClick={handleBall}>Ball</button>
+        <button onClick={handleStrike}>Strike</button>
+        <button onClick={handleFoul}>Foul</button>
+        <button onClick={handleHit}>Hit</button>
+      </section>
     </div>
   );
 }
